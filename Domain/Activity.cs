230d22620dbb98bee44075ad0f2874839eda19b6
Kezota <keziameilany2005@gmail.com>
2024-04-08 19:29:40 +0700

    fix DataContext on Persistence

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    internal class Activity
+    public class Activity
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
 
 namespace Persistence
 {
-    internal class DataContext
+    public class DataContext: DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Activity> Activities { get; set; }
     }
 }
