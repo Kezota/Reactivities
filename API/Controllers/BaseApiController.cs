99e36947331919bf20953308d06d399119495d3f
Kezota <keziameilany2005@gmail.com>
2024-04-09 14:49:23 +0700

    connect database and backend

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // api/baseapi
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
